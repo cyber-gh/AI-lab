e98e9fa24c02f68acd87568038d909836876e5c1
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2020-03-01 22:20:08 +0200

    finished prob3.trash

--- lab3/prob3.py
+++ lab3/prob3.py
@@ -1,7 +1,11 @@
 import random
 
+
 class Elev:
     counter = 0
+
+    def __repr__(self):
+        return "nume={} sanatate={} inteligenta={} oboseala={} buna_dispozitie={}".format(self.nume, self.sanatate, self.inteligenta, self.oboseala, self.bun_dispozitie)
 
     def __init__(self, nume=None, sanatate=90, inteligenta=20, oboseala=0, buna_dispozitie=100):
         self.nume = "Necunostcut_" + str(Elev.counter) if nume is None else nume
@@ -14,22 +18,29 @@ class Elev:
         self.activites = dict()
         self.is_active = True
         self.coeficient = 1.0
+        self.has_activity = False
         Elev.counter += 1
 
     def apply_activity(self, activity):
+        self.has_activity = True
         self.activites[activity.nume] = 0
         self.current_activity = activity
         self.executed_time = 0
 
+    def needs_new_activity(self):
+        return self.has_activity == False
+
     def execute_activity(self):
         self.executed_time += 1
-        self.activites[self.current_activity.name] += 1
+        self.activites[self.current_activity.nume] += 1
 
         self.sanatate -= self.current_activity.factor_sanatate / self.current_activity.durata
         self.inteligenta += self.current_activity.factor_inteligenta / self.current_activity.durata * self.coeficient
         self.oboseala += self.current_activity.factor_oboseala / self.current_activity.durata * self.coeficient
         self.bun_dispozitie += self.current_activity.factor_dispozitie / self.current_activity.durata * self.coeficient
-
+        self.get_state()
+        if self.executed_time >= self.current_activity.durata:
+            self.has_activity = False
         return self.executed_time <= self.current_activity.durata
 
     def is_final(self):
@@ -56,6 +67,11 @@ class Elev:
         else:
             self.coeficient = 1.0
 
+        if self.inteligenta >= 100:
+            return "Elevul a absolvit"
+
+        return "Elevul continua"
+
     def show_activities(self):
         for key, value in self.activites.items():
             print("activity={} executat={}".format(key, value))
@@ -64,11 +80,11 @@ class Elev:
 class Activitate:
 
     def __init__(self, nume, factor_sanatate, factor_inteligenta, factor_oboseala, factor_dispozitie, durata):
-        self.durata = durata
-        self.factor_dispozitie = factor_dispozitie
-        self.factor_oboseala = factor_oboseala
-        self.factor_inteligenta = factor_inteligenta
-        self.factor_sanatate = factor_sanatate
+        self.durata = float(durata)
+        self.factor_dispozitie = float(factor_dispozitie)
+        self.factor_oboseala = float(factor_oboseala)
+        self.factor_inteligenta = float(factor_inteligenta)
+        self.factor_sanatate = float(factor_sanatate)
         self.nume = nume
 
     def __str__(self):
@@ -99,6 +115,14 @@ if __name__ == '__main__':
         pause_time = current_time + nr
         while current_time < pause_time:
             for student in students:
-                student.apply_activity(random.choice())
-
-
+                if student.needs_new_activity():
+                    student.apply_activity(random.choice(activities))
+                student.execute_activity()
+                print(student)
+                student.show_activities()
+                print()
+                print("  " + student.get_state())
+            current_time += 1
+            if current_time >= 22:
+                current_time = 6
+                pause_time = 6
